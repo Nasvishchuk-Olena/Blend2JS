d2c2d4e95c42a17f4557f7cf5f30c9ac7fa86ae3
Kirill Voronin <voroninkv09@gmail.com>
2022-12-16 21:06:54 +0200

    add ternary operator

--- index.js
+++ index.js
@@ -6,9 +6,13 @@
 //у противному випадку показати:"Не знаєте? ECMAScript!"
 
 const promptName = prompt("Яка офіційна назва JavaScript?");
-if (promptName === "ECMAScript") {alert("Вірно!");
-    
-} else {
-    alert("Не знаєте? ECMAScript!");
-    
-}
+// if (promptName === "ECMAScript") {alert("Вірно!");
+
+// } else {
+//     alert("Не знаєте? ECMAScript!");
+
+// }
+//promptName === "ECMAScript" ? alert("Вірно!") : alert("Не знаєте? ECMAScript!");
+const message =
+  promptName === "ECMAScript" ? "Вірно!" : "Не знаєте? ECMAScript!";
+alert(message);
