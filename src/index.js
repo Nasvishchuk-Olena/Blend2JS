bab2bb446589684a8089f7e3a4dde71a95754f20
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-03-10 21:18:07 +0100

    productId

--- src/index.js
+++ src/index.js
@@ -1,23 +1,47 @@
-import './styles/normalize.css';
-import './styles/index.css';
-import './requests/products';
-import { getProducts } from './requests/products';
+import "./styles/normalize.css";
+import "./styles/index.css";
+import "./requests/products";
+import { getProducts, getProductsById } from "./requests/products";
 
-const allProductsEl = document.querySelector('#allProducts');
+// const allProductsEl = document.querySelector("#allProducts");
 
-async function createMarkup() {
-  const { data } = await getProducts();
-  const markup = data.products
-    .map(
-      product =>
-        `<li class="product">
+// async function createMarkup() {
+//   const { data } = await getProducts();
+//   const markup = data.products
+//     .map(
+//       (product) =>
+//         `<li class="product">
+//           <h2 class="title">${product.title}</h2>
+//           <p class="price">${product.price}</p>
+//           <p class="description">${product.description}</p>
+//         </li>`
+//     )
+//     .join("");
+//   allProductsEl.innerHTML = markup;
+// }
+
+// createMarkup();
+
+const singleProductCard = document.querySelector("#singleProduct");
+const singleProductForm = document.querySelector("#singleProductForm");
+
+singleProductForm.addEventListener("submit", singleProductFormSubmitHandler);
+
+function singleProductFormSubmitHandler(e) {
+  e.preventDefault();
+  const productIdForSearch = e.target.id.value;
+  renderProductCard(productIdForSearch);
+  e.target.reset();
+}
+
+async function renderProductCard(productId) {
+  const { data: product } = await getProductsById(productId);
+  const markup = `
           <h2 class="title">${product.title}</h2>
           <p class="price">${product.price}</p>
           <p class="description">${product.description}</p>
-        </li>`
-    )
-    .join('');
-  allProductsEl.innerHTML = markup;
-}
+          <img src="${product.images[0]}"/>
+        `;
 
-createMarkup();
+  singleProductCard.innerHTML = markup;
+}
--- src/requests/products.js
+++ src/requests/products.js
@@ -1,6 +1,11 @@
-import { instance } from '../services/api';
+import { instance } from "../services/api";
 
 export async function getProducts() {
-  const data = await instance.get('/products');
+  const data = await instance.get("/products");
   return data;
+}
+
+export async function getProductsById(productId) {
+  const product = await instance.get(`/products/${productId}`);
+  return product;
 }
