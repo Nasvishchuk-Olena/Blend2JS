c37033aaa7f80db7d4e74c591036b6d340ecfa58
Alex <103459735+torn80beta@users.noreply.github.com>
2023-03-10 22:58:38 +0200

    task3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import "./styles/normalize.css";
-import "./styles/index.css";
-import "./requests/products";
-import { getProducts, getProductsById } from "./requests/products";
+import './styles/normalize.css';
+import './styles/index.css';
+import './requests/products';
+import { getProducts, getProductsById, addProduct } from './requests/products';
 
 // const allProductsEl = document.querySelector("#allProducts");
 
@@ -22,26 +22,48 @@ import { getProducts, getProductsById } from "./requests/products";
 
 // createMarkup();
 
-const singleProductCard = document.querySelector("#singleProduct");
-const singleProductForm = document.querySelector("#singleProductForm");
+// const singleProductCard = document.querySelector('#singleProduct');
+// const singleProductForm = document.querySelector('#singleProductForm');
 
-singleProductForm.addEventListener("submit", singleProductFormSubmitHandler);
+// singleProductForm.addEventListener('submit', singleProductFormSubmitHandler);
 
-function singleProductFormSubmitHandler(e) {
+// function singleProductFormSubmitHandler(e) {
+//   e.preventDefault();
+//   const productIdForSearch = e.target.id.value;
+//   renderProductCard(productIdForSearch);
+//   e.target.reset();
+// }
+
+// async function renderProductCard(productId) {
+//   const { data: product } = await getProductsById(productId);
+//   const markup = `
+//           <h2 class="title">${product.title}</h2>
+//           <p class="price">${product.price}</p>
+//           <p class="description">${product.description}</p>
+//           <img src="${product.images[0]}"/>
+//         `;
+
+//   singleProductCard.innerHTML = markup;
+// }
+
+const newProductFormEl = document.querySelector('#newProductForm');
+const newProductSectionEl = document.querySelector('#newProductSection');
+newProductFormEl.addEventListener('submit', addNewProduct);
+
+async function addNewProduct(e) {
   e.preventDefault();
-  const productIdForSearch = e.target.id.value;
-  renderProductCard(productIdForSearch);
-  e.target.reset();
-}
+  const title = e.target.title.value;
+  const description = e.target.description.value;
+  const price = e.target.price.value;
+  const { data: newProduct } = await addProduct({ title, description, price });
+  console.log(newProduct);
 
-async function renderProductCard(productId) {
-  const { data: product } = await getProductsById(productId);
   const markup = `
-          <h2 class="title">${product.title}</h2>
-          <p class="price">${product.price}</p>
-          <p class="description">${product.description}</p>
-          <img src="${product.images[0]}"/>
+          <h2 class="title">${newProduct.title}</h2>
+          <p class="price">${newProduct.price}</p>
+          <p class="description">${newProduct.description}</p>
         `;
 
-  singleProductCard.innerHTML = markup;
+  newProductSectionEl.innerHTML = markup;
+  e.target.reset();
 }
